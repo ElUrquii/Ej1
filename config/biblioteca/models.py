30b7a35a2417551f6b03c341d702de2c9713289f
ElUrquii <urquizamaxi099@gmail.com>
2020-06-23 16:52:41 -0300

    Custom Admin 2

--- config/biblioteca/models.py
+++ config/biblioteca/models.py
@@ -3,32 +3,33 @@ from django.db import models
 # Create your models here.
 
 class Autor(models.Model):
-    nombre=models.CharField(max_length=20)
-    codigo=models.AutoField(primary_key=True)
-    def str(self):
-        return '{}'.format(self.nombre)
+    nombre = models.CharField(max_length=30)
+    codigo = models.AutoField(primary_key = True)
+    def __str__(self):
+        return str(self.nombre)
 
 class Libro(models.Model):
-    titulo=models.CharField(max_length=20)
-    editorial=models.CharField(max_length=20)
-    paginas=models.IntegerField
-    codigo=models.AutoField(primary_key=True)
-    autor=models.ForeignKey(Autor, on_delete=models.CASCADE)
-    def str(self):
-        return '{}'.format(self.titulo)
+    codigo = models.AutoField(primary_key = True)
+    titulo = models.CharField(max_length = 30)
+    editorial = models.CharField(max_length = 30)
+    paginas = models.IntegerField(default = '0')
+    autor = models.ForeignKey(Autor, on_delete = models.CASCADE)
+    def __str__(self):
+        return str(self.titulo)
 
 class Ejemplar(models.Model):
-    localizacion=models.CharField(max_length=20)
-    codigo=models.AutoField(primary_key=True)
-    libro=models.ForeignKey(Libro, on_delete=models.CASCADE)
-    def str(self):
-        return '{}'.format(self.libro)
+    codigo = models.AutoField(primary_key = True)
+    localizacion = models.CharField(max_length = 30)
+    libro = models.ForeignKey(Libro, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return str('Ejemplar ' + str(self.codigo) + ' de ' + str(self.libro))
 
 class Usuario(models.Model):
-    nombre=models.CharField(max_length=20)
-    telefono=models.IntegerField()
-    direccion=models.CharField(max_length=20)
-    codigo=models.AutoField(primary_key=True)
-    ejemplares=models.ManyToManyField(Ejemplar)
-    def str(self):
-        return '{}'.format(self.nombre)
+    telefono = models.IntegerField()
+    codigo = models.AutoField(primary_key = True)
+    nombre = models.CharField(max_length=30)
+    direccion = models.CharField(max_length = 30)
+    ejemplar = models.ManyToManyField(Ejemplar)
+    def __str__(self):
+        return str(self.nombre)
